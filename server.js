0c4540e00de4987484bac3c580553afe51fb905f
neerfix <nicolas.notararigo@gmail.com>
2022-06-18 12:35:52 +0200

    feat: add sentry

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 'use strict';
-
 const express = require("express");
 const {createServer} = require("http");
 const {Server} = require("socket.io");
 const app = express();
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 const ent = require('ent');
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */});
+const io = new Server(httpServer, {
+  cors: {
+    origins: ['http://localhost:4200']
+  }
+});
+
+Sentry.init({
+  dsn: "https://ac5eefb263564a459ed5fb089aceecd2@o1064146.ingest.sentry.io/6511891",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+
 app.set('view engine', 'ejs');
 const users = [];
 
@@ -40,13 +68,29 @@ io.on("connection", (socket) => {
     socket.broadcast.emit(socket.id, {message: username + ' is disconnected '});
   });
 
+  app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+
   // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
   socket.on('tab-general', function (username, toUserId, fromUserId, message, partyId) {
-    message = ent.encode(message);
+    console.log(message);
+    console.log(username);
     users.push({username, fromUserId});
 
     socket.broadcast.emit('message_' + partyId,{username, toUserId, fromUserId, message, partyId});
   });
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 httpServer.listen(3000);
